f9a4e109e45541a00e35d9f4c2f7272b4d31864f
Lim DongGyu <ldg1160296@gmail.com>
2022-12-08 23:35:39 +0900

    cm

---
+++ src/main/java/com/sparta/dglimvlog/DglimvlogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.dglimvlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DglimvlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DglimvlogApplication.class, args);
+    }
+
+}
